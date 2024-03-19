db1f9ba7fb6c78b8b98bbd080567293587207d21
sidh2901 <siddheshrajendranaik@gmail.com>
2024-03-18 23:10:25 -0400

    changes w.r.t. prom-client

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const client = require("prom-client");
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 const { PubSub } = require("@google-cloud/pubsub");
@@ -9,6 +10,39 @@ app.use(cors());
 
 app.use(express.json());
 
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+// Metrics endpoint
+const register = new client.Registry();
+collectDefaultMetrics({ register });
+
+// Define custom metrics
+const pubSubLatencyHistogram = new client.Histogram({
+  name: "pub_sub_latency",
+  help: "Histogram of Pub/Sub latency",
+  labelNames: ["method"],
+  buckets: [0.1, 5, 15, 50, 100, 500], // Example buckets in milliseconds
+});
+const individualLatencyGauge = new client.Gauge({
+  name: "individual_pub_sub_latency",
+  help: "Individual Pub/Sub latency",
+});
+
+const totalLatencyHistogram = new client.Histogram({
+  name: "total_latency",
+  help: "Histogram of total processing latency",
+  labelNames: ["method"],
+  buckets: [0.1, 5, 15, 50, 100, 500], // Example buckets in milliseconds
+});
+const totalLatencyGauge = new client.Gauge({
+  name: "api_latency",
+  help: "Api latency",
+});
+
+register.registerMetric(pubSubLatencyHistogram);
+register.registerMetric(totalLatencyHistogram);
+register.registerMetric(totalLatencyGauge);
+register.registerMetric(individualLatencyGauge);
 // Initialize Pub/Sub client
 const pubSubClient = new PubSub({
   projectId: "notifyspherepoc",
@@ -81,10 +115,10 @@ app.post("/api/submitOrder", async (req, res) => {
       `${pubSubLatency} ms`,
       `${totalLatency} ms`
     );
-
-    // Calculate total round-trip latency
-    // This requires that the client sends `requestStartTime`
-
+    pubSubLatencyHistogram.observe(pubSubLatency);
+    totalLatencyHistogram.observe(totalLatency);
+    individualLatencyGauge.set(pubSubLatency);
+    totalLatencyGauge.set(totalLatency);
     res.status(200).json({
       message: "Order successfully published",
       orderId: result.lastID,
@@ -97,6 +131,10 @@ app.post("/api/submitOrder", async (req, res) => {
       .status(500)
       .json({ error: `Error processing your order: ${error.message}` });
   }
+});
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
 });
 
 app.listen(PORT, () => console.log(`API service running on port ${PORT}`));
